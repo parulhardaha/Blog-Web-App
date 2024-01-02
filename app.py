9def84b6098fcaba1cb0488220cddd9d6dbc051f
parulhardaha <phardaha5@gmail.com>
2024-01-02 22:39:44 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+#create a Flask Instance
+app = Flask(__name__)
+#app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+#ceate a Route decorator url
+# http://127.0.0.1:5000/
+@app.route('/')
+def index():
+  first_name="Parul"
+  my_list=["Pizza","Burger","Pani puri",36,"Capchinno"]
+  stuff=" This is <strong>This text is important!</strong> Text"
+  return render_template("index.html", first_name=first_name, stuff=stuff, my_list=my_list)
+
+#localhost:5000/user/Parul
+@app.route('/user/<name>')
+def user(name):
+  return render_template("user.html",user_name=name)
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template("404.html"),404
+
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+ return render_template("500.html"),500
