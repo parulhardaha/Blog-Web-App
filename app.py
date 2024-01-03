23fcdd28a90303ef1114f12b5c2a17df232dbe0d
parulhardaha <phardaha5@gmail.com>
2024-01-03 17:55:16 +0530

    Added Forms and Navbar

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #create a Flask Instance
 app = Flask(__name__)
 #app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SECRET_KEY'] = "my super secret key is here"
+
+#Create Form Class
+class NamerForm(FlaskForm):
+   name= StringField ("Enter your Name here", validators=[DataRequired()])
+   submit= SubmitField("Submit")
+
 
 #ceate a Route decorator url
 # http://127.0.0.1:5000/
@@ -27,3 +37,16 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
  return render_template("500.html"),500
+
+#Create Nmae Page
+@app.route('/name',methods=['GET','POST'])
+def name():
+  name=None
+  form=NamerForm()
+  #Validate Form
+  if form.validate_on_submit():
+     name=form.name.data
+     form.name.data=''
+     flash("Form Submitted Successfully!")
+
+  return render_template("name.html",name=name,form=form)
